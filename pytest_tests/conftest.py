b61a56845a41e260aa1546d1eb58869be3f0caba
Ramu <ramukusupudi@gmail.com>
2025-05-06 15:20:24 -0500

    Update conftest.py

--- pytest_tests/conftest.py
+++ pytest_tests/conftest.py
@@ -1,2 +1,18 @@
 
 # Shared pytest fixtures can be added here
+import pytest
+
+# Shared pytest fixtures can be added here
+
+@pytest.fixture(scope="class")
+def set():
+    print("")
+    yield
+    print("")
+
+
+def dataLoad():
+    print("user profiles data is being created")
+    return ["Ramu","kusupudi","learning Fixtures"]
+
+
